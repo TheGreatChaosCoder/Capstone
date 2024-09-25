b14c61e3351ad4b59fe1e5560b21afdb428054b3
TheGreatChaosCoder <thegreatchaoshunter@gmail.com>
2024-09-25 15:19:53 -0500

    init commit

---
+++ input_listener.c
@@ -0,0 +1,11 @@
+/**
+ * @file input_listener.c
+ * @author Connor Johnson
+ * @brief Inplements interrupts for button inputs
+ *        and periodic data reading
+ * @version 0.1
+ * @date 2024-09-25
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
---
+++ main.c
@@ -0,0 +1,10 @@
+/**
+ * @file main.c
+ * @author Connor Johnson
+ * @brief The main file that will be ran at the start.
+ * @version 0.1
+ * @date 2024-09-25
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
